df29551b3a03be78a91c4fc216ebaabb6a344cb5
Sara-Vass <svasscs@gmail.com>
2019-06-12 14:48:36 -0700

    aws error

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class User(db.Model):
     def __repr__(self): 
         """ Provide helpful representation when printed """
 
-        return f"<User user_id = {self.user_id} email = {self.email}>"
+        return f"""<User user_id = {self.user_id} email = {self.email}>"""
 
     def to_dict(self):
         """returns a dictionary representation of the instance"""
