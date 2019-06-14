87724179e1f1fd6bdaa2298895d38375c22d76b2
Sara-Vass <svasscs@gmail.com>
2019-06-13 19:57:19 -0700

    made hompage map of all items

---
+++ static/js/homepage-map.js
@@ -0,0 +1,132 @@
+"use strict";
+
+var map;
+
+function initMap() {
+
+    // console.dir(userLocation);
+
+    let mapCenter = { lat: 37.73, lng: -122.453526 };
+
+    //try to use geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        console.dir(pos);
+
+        const userMarker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            icon: {
+                url: '/static/user-location.svg',
+                scaledSize: new google.maps.Size(30, 30)
+            }
+        });
+
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false);
+    }
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: mapCenter,
+      zoom: 11
+    });
+
+    const infoWindow = new google.maps.InfoWindow();
+
+    // //AJAX call to get list of founds
+    $.get('/api/found', (founds) => {
+        for(let found of founds) {
+            const marker = new google.maps.Marker({
+                position: new google.maps.LatLng(found.lat, found.lng),
+                map: map,
+                title: found.title,
+                icon: {
+                    url: '/static/img/found-marker.svg',
+                    scaledSize: new google.maps.Size(30, 30)
+                }
+                });
+
+                // Define the content of the infoWindow
+                const html = `<div class="window-content">
+                <h3>
+                    <a href="found/${found.id}">
+                        ${found.title}
+                    </a>
+                </h3>
+                <ul class="found-info">
+                  <li><b>Description: </b>${found.description}</li>
+                  <li><b>Found By: </b>
+                    <a href=${"/user/" + found.user_id}>
+                        ${found.user_fname} ${found.user_lname}
+                    </a></li>
+                </ul>
+                </div>`;
+
+              google.maps.event.addListener(marker, 'click', () => {
+                infoWindow.close();
+                infoWindow.setContent(html);
+                infoWindow.open(map, marker);
+
+
+
+            });
+            
+        }
+    });
+
+     // //AJAX call to get list of losts
+    $.get('/api/lost', (losts) => {
+        for(let lost of losts) {
+            const marker = new google.maps.Marker({
+                position: new google.maps.LatLng(lost.lat, lost.lng),
+                map: map,
+                title: lost.title,
+                icon: {
+                    url: '/static/img/lost-marker.svg',
+                    scaledSize: new google.maps.Size(30, 30)
+                }
+            });
+            
+            // Define the content of the infoWindow
+            const html = `<div class="window-content">
+                <h3>
+                    <a href="lost/${lost.id}">
+                        ${lost.title}
+                    </a>
+                </h3>
+                <ul class="found-info">
+                  <li><b>Description: </b>${lost.description}</li>
+                  <li><b>Found By: </b>
+                    <a href=${"/user/" + lost.user_id}>
+                        ${lost.user_fname} ${lost.user_lname}
+                    </a></li>
+                  <li><b>Reward: </b>$${lost.reward}</li>
+                </ul>
+                </div>`;
+
+            google.maps.event.addListener(marker, 'click', () => {
+                infoWindow.close();
+                infoWindow.setContent(html);
+                infoWindow.open(map, marker);
+
+            });
+
+        }
+    });
+}
+
+function handleLocationError(browserHasGeolocation) {
+    console.log(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+}
